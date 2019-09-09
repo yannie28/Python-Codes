24f305819248ecfffbd9b2f7d052112b84d02fc3
ariannetan <aj.yannie@gmail.com>
2019-09-09 19:43:01 +0800

    bubbleSort and reverseOrder

---
+++ bubbleSort.py
@@ -0,0 +1,37 @@
+#bubble sort in increasing order
+lists = [2,3,4,6,5]
+flag = True
+passes = 0
+swap = 0
+
+while flag:
+    passes += 1
+    flag = False
+    for i in range(len(lists)-1):
+        if lists[i] > lists[i+1]:
+            flag = True
+            swap += 1
+            lists[i], lists[i+1] = lists[i+1], lists[i]
+            
+print("Increasing Order: ", lists)
+print("No of passes: ", passes)
+print("No of swaps: ", swap, end="\n\n")
+
+#bubble sort in decreasing order
+lists = [2,3,4,6,5]
+flag = True
+passes = 0
+swap = 0
+
+while flag:
+    passes += 1
+    flag = False
+    for i in range(len(lists)-1):
+        if lists[i] < lists[i+1]:
+            flag = True
+            swap += 1
+            lists[i], lists[i+1] = lists[i+1], lists[i]
+            
+print("Decreasing Order: ", lists)
+print("No of passes: ", passes)
+print("No of swaps: ", swap)
---
+++ reverseOrder.py
@@ -0,0 +1,16 @@
+#Arianne Tan - 9/9/2019
+#printing myList in reverse order
+
+myList = [10, 1, 8, 3, 5]
+length = len(myList)
+
+for i in range(length // 2):
+    myList[i], myList[length - i - 1] = myList[length - i - 1], myList[i]
+
+print(myList)
+
+#alternative solution
+for i in range(length // 2):
+    myList[i], myList[i-(2*i+1)] = myList[i-(2*i+1)], myList[i]
+
+print(myList)
