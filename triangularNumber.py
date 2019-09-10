32455a0a435477855acaa2452bf529a207f42d6a
ariannetan <aj.yannie@gmail.com>
2019-09-10 09:14:37 +0800

    Update triangularNumber.py

--- triangularNumber.py
+++ triangularNumber.py
@@ -1,9 +1,11 @@
-#check if the number is a triangular number (Summation of i to n where i is 0) and output n (maximum height of the triangle)
+import math
+#check if the positive number is a triangular number (Summation of i to n where i is 0) and output n (maximum height of the triangle)
 #Input=0; Output=True
 #Input=8; Output=False
 #Input=10; Output=True
 
-number = 0#given
+number = 11#given
+print("Given number:",number,end="\n\n")
 
 #Solution no 1
 #Run time: O(n) or Linear
@@ -16,5 +18,17 @@ while total<=number:
     i+=1
     total+=i
 print(answer)
-print("Maximum height of the triangle:", i)
+print("Maximum height of the triangle:", i-1)
+print("Excess of",number%(total-i),end="\n\n")
 
+#Solution no 2
+#Run time: O(1) or Constant
+#number = (n(n+1))/2
+answer = "Not a Triangular Number"
+n = (-1+math.sqrt(1+4*2*number))/2 #solving for n using quadratic formula
+total = math.floor(n)*(math.floor(n)+1)//2
+if math.floor(n) == n:
+    answer = "A Triangular Number"
+print(answer)
+print("Maximum height of the triangle:", math.floor(n))
+print("Excess of",number%total)
