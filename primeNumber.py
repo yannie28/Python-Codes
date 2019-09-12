4acbf31b15be876b9dbb515c342891241ff7078b
ariannetan <aj.yannie@gmail.com>
2019-09-12 15:08:35 +0800

    Update primeNumber.py

--- primeNumber.py
+++ primeNumber.py
@@ -1,3 +1,7 @@
+#this program checks if the number is Prime or not
+#used theorem: if both a and b is greater than the square root of n, then a*b is greater than n
+#hence, there should be one factor that is less than the square root of n
+
 import math
 
 def isPrime(number):
