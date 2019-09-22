e8edbb757a5ef4ca233ee84f28383257a6a2676e
ariannetan <aj.yannie@gmail.com>
2019-09-22 09:42:38 +0800

    update sample package and modules

---
+++ Sample/progs/main.py
@@ -0,0 +1,12 @@
+# main.py
+from sys import path
+path.append("..\\modules")
+
+import module
+
+print(module.__counter)
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))
