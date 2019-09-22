9c2b7f71f8171d264cdd87f3d4a672cb9bccb73d
ariannetan <aj.yannie@gmail.com>
2019-09-22 10:34:27 +0800

    packages and modules

---
+++ Sample/progs/main2.py
@@ -0,0 +1,11 @@
+from sys import path
+
+path.append('A:\\GitHub\\Python-Codes\\Sample\\packages')
+
+import extra.iota as iot
+import extra.good.best.sigma as sig 
+from extra.good.best.tau import FunT
+
+print(iot.FunI())
+print(sig.FunS())
+print(FunT())
