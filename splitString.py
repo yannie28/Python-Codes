6b95e0ec9cf8512b93c41d3d4f5656a7a8ffa20f
ariannetan <aj.yannie@gmail.com>
2019-09-23 16:03:11 +0800

    Create splitString.py

---
+++ splitString.py
@@ -0,0 +1,24 @@
+def mySplit(strings, separator=" "):
+    
+    lists = []
+    temp = ""
+
+    for i in range(len(strings)):
+        if not isSeparator(strings[i], separator):
+            temp += strings[i]
+            if i == len(strings) - 1 and temp:
+                lists.append(temp)
+        elif temp:
+            lists.append(temp)
+            temp = ""
+
+    return lists
+
+def isSeparator(strings, separator):
+    if separator == " ":
+        return strings.isspace()
+    else:
+        return strings == separator
+
+print(mySplit("To be or not to be, that is the question"))
+
