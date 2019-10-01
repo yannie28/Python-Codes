e51ba59f7332c8b2476311af06f728b2556bd51e
ariannetan <aj.yannie@gmail.com>
2019-10-01 12:52:00 +0800

    Update firstPosInteger.py

--- firstPosInteger.py
+++ firstPosInteger.py
@@ -11,10 +11,30 @@ def firstPos(given):
     for i in given: #traverse the given list
         if i == min+1: #check if the element is equal to the min, if yes...
             min += 1 #the minimum number will increment by 1, otherwise just continue
-    
-    if min == 0: #this is for the list with all numbers < 1
-        return min+1 #return 1
 
     return min+1
+
+#Solution 2
+#O(n) - running time
+#O(1) - space
+def firstPos2(given):
+    counter = 0 #counting number of positive numbers
+    for i in range(len(given)): #traverse the given list
+        if given[i] > 0: #check if element is greater than 0, if yes...
+            given[counter] = given[i] #place the element to the index of counter so that all the positive numbers will be at the left side of the list
+            counter += 1 #increment counter
     
-print(firstPos([1,2,3]))
+    del given[counter:] #delete all negative and zeroes | all the elements to the right of the counter
+
+    for i in given: #traverse the list
+        x = abs(i) #get the absolute value of the element
+        if x <= len(given) and given[x-1] > 0: #check if the element is greater than or equal to the length of list, if yes...
+            given[x-1] = -given[x-1] #change the value of the element with index x to negative, to mark the index x that the positive element is found
+    
+    for i in given: #traverse the list
+        if i > 0: #check the first positive number in the list
+            return given.index(i)+1 #return the first missing positive integer
+
+    return len(given)+1
+    
+print(firstPos2([0,0,0,3,2,1,8,9,5,10,4,-1,6,7]))
